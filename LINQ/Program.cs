5b5b4407186e0342c7679b531865bf692d63df6f
alexnesbitt18 <alexnesbitt18@gmail.com>
2023-09-13 11:00:16 -0700

    Completed Exercise by Ordering VideoGames

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videogames = new List<string>() { "Axe", "Starfield", "Falling", "Zelda", "Halo"};
+            var ordervideogames = videogames.OrderBy(name => name.Length);
+            foreach(var game in ordervideogames)
+            {
+                Console.WriteLine(game);
+            }
+
         }
     }
 }
